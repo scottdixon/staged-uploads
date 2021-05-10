3098194074e354cd9e326f92ef82e92215722310
Scott Dixon <scott.dixon@shopify.com>
2021-05-10 14:28:24 +1000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Heading, Page } from "@shopify/polaris";
+import ResourceListWithCollections from '../components/ResourceListWithCollections';
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+  </Page>
+);
+
+export default Index;
